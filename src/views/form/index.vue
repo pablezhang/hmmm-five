5897731bb303002ba5d564976bb0054af8cc685d
sola <sora2266@163.com>
2023-06-19 17:07:48 +0800

    feat：静态页面

--- src/views/form/index.vue
+++ src/views/form/index.vue
@@ -1,49 +1,36 @@
 <template>
-  <div class="app-container">
-    <el-form ref="form" :model="form" label-width="120px">
-      <el-form-item label="Activity name">
-        <el-input v-model="form.name" />
-      </el-form-item>
-      <el-form-item label="Activity zone">
-        <el-select v-model="form.region" placeholder="please select your zone">
-          <el-option label="Zone one" value="shanghai" />
-          <el-option label="Zone two" value="beijing" />
-        </el-select>
-      </el-form-item>
-      <el-form-item label="Activity time">
-        <el-col :span="11">
-          <el-date-picker v-model="form.date1" type="date" placeholder="Pick a date" style="width: 100%;" />
+  <div>
+    <el-card>
+      <el-row>
+        <el-col class="search" :span="6">关键字 <el-input v-model="title" placeholder="根据文章标题搜索" /></el-col>
+        <el-col class="search" :span="6">状态
+          <el-select placeholder="请选择">
+            <el-option label="启用" value="shanghai" />
+            <el-option label="禁用" value="beijing" />
+          </el-select>
         </el-col>
-        <el-col :span="2" class="line">-</el-col>
-        <el-col :span="11">
-          <el-time-picker v-model="form.date2" type="fixed-time" placeholder="Pick a time" style="width: 100%;" />
+        <el-col :span="6">
+          <el-button>清除</el-button>
+          <el-button type="primary">搜索</el-button>
         </el-col>
-      </el-form-item>
-      <el-form-item label="Instant delivery">
-        <el-switch v-model="form.delivery" />
-      </el-form-item>
-      <el-form-item label="Activity type">
-        <el-checkbox-group v-model="form.type">
-          <el-checkbox label="Online activities" name="type" />
-          <el-checkbox label="Promotion activities" name="type" />
-          <el-checkbox label="Offline activities" name="type" />
-          <el-checkbox label="Simple brand exposure" name="type" />
-        </el-checkbox-group>
-      </el-form-item>
-      <el-form-item label="Resources">
-        <el-radio-group v-model="form.resource">
-          <el-radio label="Sponsor" />
-          <el-radio label="Venue" />
-        </el-radio-group>
-      </el-form-item>
-      <el-form-item label="Activity form">
-        <el-input v-model="form.desc" type="textarea" />
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="onSubmit">Create</el-button>
-        <el-button @click="onCancel">Cancel</el-button>
-      </el-form-item>
-    </el-form>
+        <el-col :span="6" style=" display: flex; justify-content:end">
+          <el-button style="margin-left:200px" type="success">
+            <i class="el-icon-edit" />新增技巧</el-button>
+        </el-col>
+      </el-row>
+      <el-row style="margin:10px 0">
+        <el-alert title="数据一共有几条" type="info" show-icon />
+      </el-row>
+      <el-table>
+        <el-table-column label="序号" />
+        <el-table-column label="文章标题" />
+        <el-table-column label="阅读数" />
+        <el-table-column label="录入人" />
+        <el-table-column label="录入时间" />
+        <el-table-column label="状态" />
+        <el-table-column label="操作" />
+      </el-table>
+    </el-card>
   </div>
 </template>
 
@@ -51,35 +38,24 @@
 export default {
   data() {
     return {
-      form: {
-        name: '',
-        region: '',
-        date1: '',
-        date2: '',
-        delivery: false,
-        type: [],
-        resource: '',
-        desc: ''
-      }
-    }
-  },
-  methods: {
-    onSubmit() {
-      this.$message('submit!')
-    },
-    onCancel() {
-      this.$message({
-        message: 'cancel!',
-        type: 'warning'
-      })
+      title: ''
     }
   }
 }
 </script>
 
-<style scoped>
-.line{
-  text-align: center;
+<style lang='scss' scoped>
+.el-card{
+  margin: 20px;
+  height: 800px;
+  .search{
+    display: flex;
+    justify-content: center;
+    line-height: 38px;
+    .el-input{
+      margin-left: 10px;
+      width: 70%;
+    }
+  }
 }
 </style>
-
