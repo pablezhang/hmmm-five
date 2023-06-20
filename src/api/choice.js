e419e4f373b780be8af4fd2a2a346dce66d7d7a6
wolf <2857011554@qq.com>
2023-06-20 12:07:52 +0800

    feat:精选题库--试题预览

--- src/api/choice.js
+++ src/api/choice.js
@@ -1,3 +1,21 @@
 import request from '@/utils/request'
 
 export const getQuestionsChoiceAPI = (params) => { return request({ url: '/questions/choice', method: 'get', params }) }
+
+// 获取学科简单列表
+export const getSubjectsSimpleAPI = (params) => { return request({ url: '/subjects/simple', method: 'get', params }) }
+
+// 获取目录列表
+export const getDirectorysAPI = (params) => { return request({ url: '/directorys', method: 'get', params }) }
+
+// 获取标签列表
+export const getTagsAPI = (params) => { return request({ url: '/tags', method: 'get', params }) }
+
+// 获取用户简单列表
+export const getUsersSimpleAPI = (params) => { return request({ url: '/users/simple', method: 'get', params }) }
+
+// 获取题库详情
+export const getQuestionsIdAPI = (id) => { return request({ url: `/questions/${id}`, method: 'get' }) }
+
+// 试题审核
+export const getQuestionsCheckAPI = (data) => { return request({ url: ` /questions/check/${data.id}`, method: 'get', data }) }
