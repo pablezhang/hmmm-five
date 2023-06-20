19c8e02fba07624017474458acceb878fe0bf18d
Mr_odd <1979736742@qq.com>
2023-06-20 10:07:30 +0800

    feat：生成第二axios实例及拦截器

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,8 +1,15 @@
 import axios from 'axios'
-import { getToken } from './auth'
+import { getToken, removeToken } from './auth'
 import { Message } from 'element-ui'
+import router from '@/router'
+import store from '@/store'
 
 const request = axios.create({
+  timeout: 5000
+})
+
+const requestTwo = axios.create({
+  baseURL: 'http://hmajax.itheima.net/',
   timeout: 5000
 })
 
@@ -28,8 +35,29 @@ request.interceptors.response.use(function(response) {
     return Promise.reject(new Error(error))
   } else {
     Message.error('登录超时,请重新登录')
+    removeToken()
+    router.push('/login')
+    store.commit('user/saveToken', null)
     return Promise.reject(new Error(error))
   }
 })
 
+// 添加请求拦截器
+requestTwo.interceptors.request.use(function(config) {
+  return config
+}, function(error) {
+  return Promise.reject(error)
+})
+
+// 添加响应拦截器
+requestTwo.interceptors.response.use(function(response) {
+  return response
+}, function(error) {
+  return Promise.reject(new Error(error))
+})
+
 export default request
+
+export {
+  requestTwo
+}
