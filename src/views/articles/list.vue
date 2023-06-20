688372ad70a55ac90eeb6c081f653e6dc82cf9a5
sola <sora2266@163.com>
2023-06-20 18:23:11 +0800

    feat：修改bug

--- src/views/articles/list.vue
+++ src/views/articles/list.vue
@@ -196,7 +196,7 @@ export default {
       this.getArticles()
     },
     onDel(id) {
-      this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+      this.$confirm('此操作将永久删除该文章, 是否继续?', '提示', {
         confirmButtonText: '确定',
         cancelButtonText: '取消',
         type: 'warning'
@@ -204,9 +204,7 @@ export default {
         await delArticlesAPI(id)
         this.$message({ type: 'success', message: '删除成功!' })
         this.getArticles()
-      }).catch(() => {
-        this.$message({ type: 'success', message: '已取消删除' })
-      })
+      }).catch(() => {})
     },
     async onPreview(id) {
       this.previewVisible = true
