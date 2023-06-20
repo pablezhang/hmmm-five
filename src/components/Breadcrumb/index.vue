2e22d249a6360c256df2a61455300c4c6bfb2adf
Mr_odd <1979736742@qq.com>
2023-06-20 12:13:03 +0800

    feat：修改面包屑颜色

--- src/components/Breadcrumb/index.vue
+++ src/components/Breadcrumb/index.vue
@@ -33,7 +33,7 @@ export default {
       const first = matched[0]
 
       if (!this.isDashboard(first)) {
-        matched = [{ path: '/dashboard', meta: { title: 'Dashboard' }}].concat(matched)
+        matched = [{ path: '/dashboard', meta: { title: '面板' }}].concat(matched)
       }
 
       this.levelList = matched.filter(item => item.meta && item.meta.title && item.meta.breadcrumb !== false)
@@ -71,8 +71,11 @@ export default {
   margin-left: 8px;
 
   .no-redirect {
-    color: #97a8be;
+    color: #fff;
     cursor: text;
   }
+}
+::v-deep .el-breadcrumb__inner a {
+  color: #fff;
 }
 </style>
