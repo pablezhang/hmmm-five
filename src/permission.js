3a54240492f39c638e62e5e757ce91c34a87a775
sola <sora2266@163.com>
2023-06-20 17:29:41 +0800

    feat：前置路由守卫

--- src/permission.js
+++ src/permission.js
@@ -2,27 +2,24 @@ import getPageTitle from '@/utils/get-page-title'
 import NProgress from 'nprogress' // progress bar
 import 'nprogress/nprogress.css' // progress bar style
 import router from './router'
+import { Message } from 'element-ui'
+import { getToken } from './utils/auth'
 
 NProgress.configure({ showSpinner: false }) // NProgress Configuration
-// const whitePath = ['/login']
+const whitePath = ['/login']
 router.beforeEach(async(to, from, next) => {
   // start progress bar
   NProgress.start()
 
-  // set page title
   document.title = getPageTitle(to.meta.title)
-  // if (whitePath.includes(to.path)) {
-  //   next()
-  //   return
-  // }
-  // if (getToken()) {
-  //   next()
-  //   return
-  // }
-  // next('/login')
-  // Message.warning('网络异常,请稍后重试')
-  next(0)
-  // determine whether the user has logged in
+  if (getToken()) {
+    return next()
+  }
+  if (whitePath.includes(to.path)) {
+    return next()
+  }
+  next('/login')
+  Message.warning('请重新登录')
 })
 
 router.afterEach(() => {
