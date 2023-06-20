6dc9bf2c620815d45943e74844090fc1e8e394a7
sola <sora2266@163.com>
2023-06-20 11:36:08 +0800

    feat：搜索

--- src/views/articles/list.vue
+++ src/views/articles/list.vue
@@ -11,8 +11,8 @@
           </el-select>
         </el-col>
         <el-col :span="6">
-          <el-button>清除</el-button>
-          <el-button type="primary">搜索</el-button>
+          <el-button @click="onClear">清除</el-button>
+          <el-button type="primary" @click="onSearch">搜索</el-button>
         </el-col>
         <el-col :span="6" style="display: flex; justify-content: end">
           <el-button style="margin-left: 200px" type="success" @click="visible=true">
@@ -20,7 +20,7 @@
         </el-col>
       </el-row>
       <el-row style="margin: 10px 0">
-        <el-alert :title="'数据一共有' + pageParams.total + '条'" type="info" show-icon />
+        <el-alert :title="'数据一共有' + total + '条'" type="info" show-icon />
       </el-row>
       <el-table :data="tableList">
         <el-table-column align="center" label="序号" width="80px">
@@ -34,7 +34,11 @@
         </el-table-column>
         <el-table-column prop="visits" label="阅读数" width="80px" />
         <el-table-column prop="username" label="录入人" width="100px" />
-        <el-table-column prop="createTime" label="录入时间" />
+        <el-table-column prop="createTime" label="录入时间">
+          <template #default="{row}">
+            {{ new Date(row.createTime).toLocaleString('zh-CN', { timeZone: 'Asia/Shanghai' }).replace(/\//g, '-') }}
+          </template>
+        </el-table-column>
         <el-table-column prop="state" label="状态" width="100px">
           <template #default="{ row }">
             {{ row.state ===1?'已启用':'已禁用' }}
@@ -42,10 +46,10 @@
         </el-table-column>
         <el-table-column label="操作">
           <template #default="{row}">
-            <el-button type="text">预览</el-button>
+            <el-button type="text" @click="onPreview(row.id)">预览</el-button>
             <el-button v-if="row.state===1" type="text" @click="onClose(row.id,0)">禁用</el-button>
             <el-button v-else type="text" @click="onOpen(row.id,1)">启用</el-button>
-            <el-button type="text" :disabled="row.state!==1?false:disabled">修改</el-button>
+            <el-button type="text" :disabled="row.state!==1?false:disabled" @click="onEdit(row.id)">修改</el-button>
             <el-button type="text" :disabled="row.state!==1?false:disabled" @click="onDel(row.id)">删除</el-button>
           </template>
         </el-table-column>
@@ -56,12 +60,13 @@
           :page-sizes="[10, 20, 40, 50]"
           :page-size="pageParams.pagesize"
           layout="prev, pager, next, jumper"
-          :total="pageParams.total"
+          :total="total"
           @current-change="onChange"
         />
       </el-row>
     </el-card>
-    <el-dialog :visible.sync="visible" title="新增文章">
+    <!-- 编辑 -->
+    <el-dialog class="el_add" :visible.sync="visible" title="新增文章">
       <el-form ref="form" label-width="100px" :model="formData" :rules="rules">
         <el-form-item label="文章标题" prop="title">
           <el-input v-model="formData.title" placeholder="请输入文章标题" />
@@ -85,6 +90,16 @@
         </el-form-item>
       </el-form>
     </el-dialog>
+    <!-- 预览 -->
+    <el-dialog class="el_look" :visible.sync="previewVisible" title="预览文章">
+      <h3>{{ detail.title }}</h3>
+      <el-row type="flex">
+        <span>{{ detail.createTime }}</span>
+        <span>{{ detail.username }}</span>
+        <span><i class="el-icon-view" style="margin:0 10px" /> {{ detail.visits }}</span>
+      </el-row>
+      <p class="p_text" v-html="detail.articleBody" />
+    </el-dialog>
   </div>
 </template>
 <script>
@@ -92,7 +107,7 @@ import 'quill/dist/quill.core.css'
 import 'quill/dist/quill.snow.css'
 import 'quill/dist/quill.bubble.css'
 import { quillEditor } from 'vue-quill-editor'
-import { addArticlesAPI, delArticlesAPI, getArticlesAPI, stateArticlesAPI } from '@/api/articles'
+import { addArticlesAPI, delArticlesAPI, detailsArticlesAPI, editArticlesAPI, getArticlesAPI, stateArticlesAPI } from '@/api/articles'
 export default {
   components: {
     quillEditor
@@ -101,13 +116,21 @@ export default {
     return {
       visible: false,
       disabled: true,
+      previewVisible: false,
       select: '',
       title: '',
       tableList: [],
+      detail: {
+        title: '',
+        createTime: '',
+        articleBody: '',
+        username: '',
+        visits: ''
+      },
+      total: 1000,
       pageParams: {
         page: 1,
-        pagesize: 10,
-        total: 100
+        pagesize: 10
       },
       formData: {
         title: '',
@@ -132,19 +155,18 @@ export default {
   watch: {
     visible(newValue) {
       if (!newValue) {
-        this.$refs.form.resetFields(0)
+        this.$refs.form.resetFields()
       }
     }
   },
   created() {
     this.getArticles()
   },
-
   methods: {
     async  getArticles() {
-      const res = await getArticlesAPI(this.pageParams)
+      const res = await getArticlesAPI({ ...this.pageParams, keyword: this.title, state: this.select })
       this.tableList = res.items
-      this.pageParams.total = res.counts
+      this.total = res.counts
     },
     onChange(page) {
       this.pageParams.page = page
@@ -156,21 +178,43 @@ export default {
       this.getArticles()
     },
     async onClose(id, state) {
-      console.log(id, state)
       await stateArticlesAPI({ id, state })
       this.$message.success('操作成功')
       this.getArticles()
     },
     async addSubmit() {
       await this.$refs.form.validate()
-      await addArticlesAPI(this.formData)
-      this.$message.success('添加成功')
+      if (this.formData.id) {
+        await editArticlesAPI(this.formData)
+      } else {
+        await addArticlesAPI(this.formData)
+      }
+      this.$message.success(this.formData.id ? '编辑成功' : '添加成功')
       this.visible = false
       this.getArticles()
     },
     async onDel(id) {
       await delArticlesAPI(id)
       this.$message.success('删除成功')
+      this.getArticles()
+    },
+    async onPreview(id) {
+      this.previewVisible = true
+      const res = await detailsArticlesAPI(id)
+      this.detail = res
+      this.detail.createTime = new Date(res.createTime).toLocaleString('zh-CN', { timeZone: 'Asia/Shanghai' }).replace(/\//g, '-')
+    },
+    async onEdit(id) {
+      this.visible = true
+      const res = await detailsArticlesAPI(id)
+      this.formData = res
+    },
+    onSearch() {
+      this.getArticles()
+    },
+    onClear() {
+      this.select = ''
+      this.title = ''
       this.getArticles()
     }
   }
@@ -190,8 +234,12 @@ export default {
     }
   }
 }
-::v-deep .el-dialog__body{
+
+::v-deep .el_add{
+  height: 100%;
+  .el-dialog__body{
   padding-bottom: 10px;
+}
 }
 .el-form{
   .el-input{
@@ -205,6 +253,15 @@ export default {
   }
  .el-dialog__headerbtn .el-dialog__close  {
     color: #fff;
+  }
+}
+
+::v-deep .el_look{
+  height: 360px;
+  .p_text{
+    padding: 10px;
+    width: 520px;
+    background: #f5f5f5;
   }
 }
 </style>
